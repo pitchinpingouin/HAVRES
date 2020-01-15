21c7320a996797e241a4774783cf4daec6211962
unknown <margauxseg1@free.fr>
2020-01-15 14:42:10 +0100

    le chat miaule

--- Assets/Toni/sunScript.cs
+++ Assets/Toni/sunScript.cs
@@ -5,23 +5,15 @@ using UnityEngine;
 public class sunScript : MonoBehaviour
 {
     [SerializeField] private float dayDuration;
-    [SerializeField] private AudioSource dayAudio;
-    [SerializeField] private AudioSource nightAudio;
-    [SerializeField] private bool dayPlaying;
-    [SerializeField] private bool nightPlaying;
+    
     public Transform sunTransform;
 
     private float timeOfDay;
-    private float angleSun;
+    public float angleSun;
 
     // Start is called before the first frame update
     void Start()
     {
-        dayAudio.Play();
-        nightAudio.Play();
-        nightAudio.volume = 0;
-        //dayAudio.Play();
-        //dayPlaying = true;
         timeOfDay = 0.0f;
     }
 
@@ -37,40 +29,5 @@ public class sunScript : MonoBehaviour
         {
             timeOfDay -= dayDuration;
         }
-
-        if (angleSun > 180 && !nightPlaying)
-        {
-            dayPlaying = false;
-            nightPlaying = true;
-            //nightAudio.Play();
-            //dayAudio.Stop();
-            StartCoroutine(StartFade(dayAudio, 10f, 0));
-            StartCoroutine(StartFade(nightAudio, 10f, 1));
-        }
-        else if (angleSun < 150 && !dayPlaying)
-        {
-            
-            dayPlaying = true;
-            nightPlaying = false;
-            //dayAudio.Play();
-            //nightAudio.Stop();
-            StartCoroutine(StartFade(dayAudio, 10f, 1));
-            StartCoroutine(StartFade(nightAudio, 10f, 0));
-            
-        }
-    }
-
-    public static IEnumerator StartFade(AudioSource audioSource, float duration, float targetVolume)
-    {
-        float currentTime = 0;
-        float start = audioSource.volume;
-
-        while (currentTime < duration)
-        {
-            currentTime += Time.deltaTime;
-            audioSource.volume = Mathf.Lerp(start, targetVolume, currentTime / duration);
-            yield return null;
-        }
-        yield break;
     }
 }
