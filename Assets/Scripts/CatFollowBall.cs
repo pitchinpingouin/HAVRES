43cdb83a8305023b44701f8ad18394dabe43fd7d
unknown <margauxseg1@free.fr>
2020-01-21 12:29:22 +0100

    on peut à peu près se baisser

--- Assets/Scripts/CatFollowBall.cs
+++ Assets/Scripts/CatFollowBall.cs
@@ -1,44 +1,55 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using OculusSampleFramework;
 
 public class CatFollowBall : MonoBehaviour
 {
-        [SerializeField] private GameObject ball;
-        private bool havebeentaken;
+    [SerializeField] private GameObject ball;
+    private bool havebeentaken;
+    [SerializeField] private AudioClip catMeowSound;
+    private AudioSource catSource;
+    private DistanceGrabbable dg;
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         havebeentaken = false;
+        catSource = GetComponent<AudioSource>();
+        dg = ball.GetComponent<DistanceGrabbable>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!havebeentaken && ball.GetComponent<OVRGrabbable>().isGrabbed)
+        if (!havebeentaken && dg.isGrabbed) //mettre distance grabbable
         {
+            //catSource.PlayOneShot(catMeowSound, 1);
             havebeentaken = true;
         }
         float dist = Vector3.Distance(transform.position, ball.transform.position);
         
-        if (havebeentaken && !ball.GetComponent<OVRGrabbable>().isGrabbed)
+        if (havebeentaken && !dg.isGrabbed) //mettre distance grabbable
         {
             if (dist > 1f)
             {
-                GetComponent<Animator>().Play("walk");
+                transform.LookAt(ball.transform);
+                transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
+                anim.Play("walk");
                 //Vector3 ballPos = new Vector3(ball.transform.position.x, ball.transform.position.y, ball.transform.position.y);
                 transform.position = Vector3.MoveTowards(transform.position, new Vector3(ball.transform.position.x, transform.position.y, ball.transform.position.z), .03f);
                 //transform.position = new Vector3(transform.position.x, 0, transform.position.y);
             }
             else
             {
-                GetComponent<Animator>().Play("idle");
+                havebeentaken = false;
+                anim.Play("idle");
                 transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
             }
         }
+
+
         
-        
-        transform.LookAt(ball.transform);
-        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y, 0);
     }
 }
---
+++ Assets/Scripts/CatSound.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatSound : MonoBehaviour
+{
+
+    [SerializeField] private AudioClip catMeowSound;
+
+    //[SerializeField] private AudioClip catRonronSound;
+
+    private AudioSource catSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        catSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void OnTriggerEnter(Collider collider)
+    {
+
+        if (collider.CompareTag("LeftHand"))
+        {
+            //catSource.clip = catRonronSound;
+            //catSource.Play();
+            //OVRInput.SetControllerVibration(0.005f, 0.1f, OVRInput.Controller.LTouch);
+            catSource.PlayOneShot(catMeowSound, 1);
+            //TriggerVibration(catMeowSound, OVRInput.Controller.LTouch);
+            TriggerVibration(catMeowSound, 0);
+        }
+        else if (collider.CompareTag("RightHand"))
+        {
+            //catSource.clip = catRonronSound;
+            //catSource.Play();
+            //OVRInput.SetControllerVibration(0.005f, 0.1f, OVRInput.Controller.RTouch);
+            catSource.PlayOneShot(catMeowSound, 1);
+
+            TriggerVibration(catMeowSound, 1);
+        }
+    }
+    /*
+    public void OnTriggerStay(Collider collider)
+    {
+        if (collider.CompareTag("LeftHand"))
+        {
+
+            //OVRInput.SetControllerVibration(0.005f, 0.1f, OVRInput.Controller.LTouch);
+            //catSource.PlayOneShot(catMeowSound, 1);
+            //TriggerVibration(catRonronSound, OVRInput.Controller.LTouch);
+            catSource.PlayOneShot(catMeowSound, 1);
+            TriggerVibration(catMeowSound, OVRInput.Controller.LTouch);
+        }
+        else if (collider.CompareTag("RightHand"))
+        {
+            //OVRInput.SetControllerVibration(0.005f, 0.1f, OVRInput.Controller.RTouch);
+            //catSource.PlayOneShot(catMeowSound, 1);
+
+            //TriggerVibration(catRonronSound, OVRInput.Controller.RTouch);
+            catSource.PlayOneShot(catMeowSound, 1);
+
+            TriggerVibration(catMeowSound, OVRInput.Controller.RTouch);
+        }
+    }*/
+    /*
+    public void OnTriggerExit(Collider collider)
+    {
+        if (collider.CompareTag("LeftHand"))
+        {
+            //catSource.Stop();
+            //OVRInput.SetControllerVibration(0, 0, OVRInput.Controller.LTouch);
+            //catSource.PlayOneShot(catMeowSound, 1);
+        }
+        else if (collider.CompareTag("RightHand"))
+        {
+            //catSource.Stop();
+            //OVRInput.SetControllerVibration(0, 0, OVRInput.Controller.RTouch);
+        }
+    }*/
+
+    public void TriggerVibration(AudioClip vibrationAudio, int hand)
+    {
+        OVRHapticsClip clip = new OVRHapticsClip(vibrationAudio);
+
+        if (hand == 0)
+        {
+            //catSource.PlayOneShot(catMeowSound, 1);
+            OVRHaptics.LeftChannel.Preempt(clip);
+        }
+        else if (hand == 1)
+        {
+            //catSource.PlayOneShot(catMeowSound, 1);
+            OVRHaptics.RightChannel.Preempt(clip);
+        }
+    }
+    
+
+}
