c5763afdfb585efeb7c0c47aebdbace59796088d
pitchinpingouin <35998664+pitchinpingouin@users.noreply.github.com>
2020-01-23 19:11:17 +0100

    need only to fix aim

--- Assets/Scripts/DrawZone.cs
+++ Assets/Scripts/DrawZone.cs
@@ -1,87 +1,100 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using OculusSampleFramework;
 using UnityEngine;
 
 public class DrawZone : MonoBehaviour
 {
-    public GameObject canvaParam;
-    public bool canDrawZone;
+   // public GameObject canvaParam;
+    private bool canDrawZone;
     public GameObject DrawMark1;
     public GameObject DrawMark2;
-    public Transform Player;
+   // public Transform Player;
 
-    private RaycastHit hit; //C'est lui que je veux
+//    private DistanceGrabber distanceGrabberScript;
 
-    public Transform centerTransform;
+    public Transform drawZoneTransform;
     private float state;
 
+    [SerializeField] private float RayLength = 50f;
 
     void Start()
     {
+  //      distanceGrabberScript = GetComponent<DistanceGrabber>();
         canDrawZone = true;
         state = 0;
     }
 
     private void ResetZone()
     {
-        centerTransform.gameObject.SetActive(false);
+        drawZoneTransform.gameObject.SetActive(false);
         DrawMark1.SetActive(false);
         DrawMark2.SetActive(false);
     }
 
     private void zoneAppears()
     {
-        centerTransform.position = DrawMark2.transform.position + 0.5f * (DrawMark1.transform.position - DrawMark2.transform.position);
-        centerTransform.localScale = ((DrawMark1.transform.position - DrawMark2.transform.position) + Vector3.up * 10.0f);
-        centerTransform.gameObject.SetActive(true);
+        drawZoneTransform.position = DrawMark2.transform.position + 0.5f * (DrawMark1.transform.position - DrawMark2.transform.position);
+        drawZoneTransform.localScale = ((DrawMark1.transform.position - DrawMark2.transform.position) + Vector3.up * 10.0f);
+        drawZoneTransform.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        //TODO:
-        ///hit = hit point du laser tiré dns distance grabber, ou le transform du point de collision svp
-        //TODO:
-            
-            
         if (canDrawZone)
         {
-
             if (state == 0)
             {
                 ResetZone();
             }
 
-            if(state == 1)
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, RayLength))
             {
-                //DrawMark2.transform.position = hit.point + 0.1f * Vector3.up;
-            }
-
-            if (OVRInput.GetDown(OVRInput.Button.Two))
-            {
+                if (hit.collider.tag == "Ground")
                 {
+                     if (state == 1)
+                     {
+                         DrawMark2.transform.position = hit.point + 0.1f * Vector3.up;
+                     }
                     
-                    if (state == 0)
-                    {
-                        state = 1;
-                        //DrawMark1.transform.position = hit.point + 0.1f * Vector3.up;
-                        //DrawMark2.transform.position = hit.point + 0.1f * Vector3.up;
-                        //DrawMark1.SetActive(true);
-                        //DrawMark2.SetActive(true);
-                    }
-                    else
-                    {
-                        state = 0;
-                    }
+                     if (OVRInput.GetDown(OVRInput.Button.Two))
+                     {
+                        if (state == 0)
+                        {
+                            
+                            state = 1;
+                            
+                            DrawMark1.transform.position = hit.point + 0.1f * Vector3.up;
+                            DrawMark2.transform.position = hit.point + 0.1f * Vector3.up;
+                            DrawMark1.SetActive(true);
+                            DrawMark2.SetActive(true);
+                            
+                        }
+                        else
+                        {
+                            state = 0;
+                        }
+                     }
+                     
+                     if (OVRInput.GetUp(OVRInput.Button.Two))
+                     {
+                         if (state > 0)
+                         {
+                             zoneAppears();
+                             state = 2;
+                         }
+                     }
+                     
                 }
             }
-
-            if (OVRInput.GetUp(OVRInput.Button.Two))
+            else
             {
-                if (state > 0)
+                if (OVRInput.GetDown(OVRInput.Button.Two))
                 {
-                    zoneAppears();
+                    state = 0;
                 }
             }
         }
