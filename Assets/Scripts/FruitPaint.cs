0d28458c454b0d07bd3656629d3b7e9bf8746309
LittleFlameMan <marc.chicourrat-poublan@telecom-sudparis.eu>
2020-02-01 23:09:19 +0100

    Ajouts du prefab de la brosse, et de la peinture à la main

--- Assets/Scripts/FruitPaint.cs
+++ Assets/Scripts/FruitPaint.cs
@@ -8,12 +8,13 @@ public class FruitPaint : MonoBehaviour
     [SerializeField]
     private GameObject tache;
     private bool havebeentaken;
+    private int refreshPaint;
    
     // Start is called before the first frame update
     void Start()
     {
         havebeentaken = false;
-
+        refreshPaint = 0;
 
     }
 
@@ -25,11 +26,30 @@ public class FruitPaint : MonoBehaviour
         {
             havebeentaken = true;
         }
+
+        if (refreshPaint < 10)
+        {
+            refreshPaint += 1;
+        }
+
+        int layerMask = 1 << 0;
+        RaycastHit hit;
+        if (GetComponent<DistanceGrabbable>().isGrabbed || GetComponent<OVRGrabbable>().isGrabbed)
+        {
+            if (Physics.Raycast(transform.position + new Vector3(0,0,0.35f), transform.TransformDirection(Vector3.forward), out hit, 0.5f,layerMask) && refreshPaint == 10)
+            {
+                Quaternion rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
+                Vector3 contactpt = hit.point;
+                GameObject stain = Instantiate(tache, new Vector3(contactpt.x, contactpt.y + 0.01f/*-0.15f*//*+ 0.05f*/, contactpt.z), rotation);
+                stain.transform.localScale = tache.transform.localScale * 0.6f;
+                refreshPaint = 0;
+            }
+        }
     }
 
-    public void OnCollisionEnter(Collision collision)
+    public void OnCollisionStay(Collision collision)
     {
-        if (havebeentaken /*&& collision.gameObject.CompareTag("Ground")*/)
+        if (havebeentaken && collision.gameObject.CompareTag("Ground"))
         {
             Destroy(gameObject);
             ContactPoint contact = collision.contacts[0];
@@ -46,5 +66,11 @@ public class FruitPaint : MonoBehaviour
         }
 
 
+    }
+
+
+    public bool GetHaveBeenTaken()
+    {
+        return havebeentaken;
     }
 }
