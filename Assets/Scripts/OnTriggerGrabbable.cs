bf82e4360026b3173c6ef89bbd22fbfedb89edab
pitchinpingouin <35998664+pitchinpingouin@users.noreply.github.com>
2020-02-03 17:36:03 +0100

    blueprint works!

--- Assets/Scripts/OnTriggerGrabbable.cs
+++ Assets/Scripts/OnTriggerGrabbable.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class OnTriggerGrabbable : MonoBehaviour
 {
+    public Material ghostMaterial;
     private List<Collider> colliders = new List<Collider>();
     private List<Vector3> massDragAngulardrag = new List<Vector3>();
 
@@ -35,15 +36,14 @@ public class OnTriggerGrabbable : MonoBehaviour
             }
             
             //Remove Gravity to the object
+
+            /*
             rgbd.useGravity = false;
             rgbd.isKinematic = true;
-
+            */
 
             rgbd.angularDrag = 9999999f;
             rgbd.drag = 99999999f;
-
-            //Remove collider of the object
-            ///rgdb.detectCollisions = false;
         }
 
     }
@@ -64,9 +64,6 @@ public class OnTriggerGrabbable : MonoBehaviour
                 rgdb.drag = massDragAngulardrag[index].y;
                 rgdb.angularDrag = massDragAngulardrag[index].z;
 
-                rgdb.useGravity = true;
-                rgdb.isKinematic = false;
-
                 //Remove object from lists
                 massDragAngulardrag.RemoveAt(index);
                 colliders.Remove(colliderExiting);
@@ -80,27 +77,40 @@ public class OnTriggerGrabbable : MonoBehaviour
         {
             GameObject empty = Instantiate(meshCombinerEmpty);
             empty.transform.position = transform.position;
+            //Empty has a meshCombiner script on it already !"
             meshCombinerScript = empty.GetComponent<MeshCombiner>();
 
-            //Set up the mesh combiner
-            ///TODO
-            ///
+            //Set up the mesh combiner's booleans
+            meshCombinerScript.DeactivateCombinedChildren = true;
+            //meshCombinerScript.CreateMultiMaterialMesh = true;
             
-            //Mettre les objets en enfant de l'empty
+            //Put each concerned object as a child of the new empty
             for(int i = 0; i < colliders.Count; i++)
             {
                 colliders[i].transform.parent = empty.transform;
             }
 
             //Combine meshes and add mass
-            meshCombinerScript.CombineMeshes(false);
+            meshCombinerScript.CombineMeshes(true);
             CalculateTotalMassAndAddRgbd(empty);
+
+            //TODO: put a meshcolldier
+            empty.GetComponent<MeshRenderer>().material = ghostMaterial;
+
+
+
+            //TODO: save the empty (who's actually full) in the prefab folder
+
             
             //Empty the two lists, and reassign the new rigidbody parameters like gravity
-            ///TODO
-            ///
+            for (int i = 0; i < colliders.Count; i++)
+            {
+                colliders[i].GetComponent<Rigidbody>().mass = massDragAngulardrag[i].x;
+                colliders[i].GetComponent<Rigidbody>().drag = massDragAngulardrag[i].y;
+                colliders[i].GetComponent<Rigidbody>().angularDrag = massDragAngulardrag[i].z;
+            }
 
-            //Sinon, ca sera vidé au Enable donc osef
+            ///Lists will be cleared OnEnable() so we don't mind here
         }
     }
 
@@ -114,9 +124,11 @@ public class OnTriggerGrabbable : MonoBehaviour
         }
 
         //Add Rigidbody to the emptyGameObject
-        emptyGameObject.AddComponent<Rigidbody>();
-        emptyGameObject.GetComponent<Rigidbody>().mass = totalMass;
-        
+        Rigidbody rgbd = emptyGameObject.AddComponent<Rigidbody>();
+        rgbd.mass = totalMass;
+        rgbd.useGravity = false;
+        rgbd.isKinematic = true;
+
         return;
     }
 }
